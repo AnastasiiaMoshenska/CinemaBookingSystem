ea8ed22d3a1ace5187a6948a5915dc1c96a1ea72
amoshens <anastasiia.moshenska@stud.hslu.ch>
2023-05-18 16:28:57 +0200

    Booking page. Seats are styled

--- application/src/components/SeatItem/SeatItem.tsx
+++ application/src/components/SeatItem/SeatItem.tsx
@@ -3,7 +3,8 @@ import styles from './SeatItem.module.scss';
 export default function SeatItem(props: any){
     return(
         <div className={styles.SeatItem}>
-            {props.seat.id}
+            <img src="src/assets/seat.svg" onLoad="SVGInject(this)" alt="seat"/>
+            <span className={styles.number}>{props.seat.id}</span>
         </div>
     )
 }
